4f3e2f03c590d62c6ab38dc96bfdf8ec0798753f
Navnath <navnyajade@gmail.com>
2026-06-23 18:42:47 +0530

    Add example of if
    Added a simple budget check to determine if a user can buy a Thar based on their input budget.

---
+++ 4-control-flow-statement/theory/if.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int budget;
+    cout << "Enter your Budget: ";
+
+    //input
+    cin >> budget;
+
+    if(budget > 2000000){
+        cout << "you can buy Thar" << endl;
+    }
+
+    cout << "you can't buy Thar";
+
+    return 0;
+}
