40ad1029f008a88f1ac40d669baedff881a7f87c
Navnath <navnyajade@gmail.com>
2026-06-23 18:33:20 +0530

    Add files via upload

---
+++ 2-variables-datatypes/theory/size.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int age = 12;
+    cout << sizeof(age) << endl;
+
+    return 0;
+}
---
+++ 2-variables-datatypes/theory/variable.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    //variable define
+    //int age = 19;
+    //cout << "My age is : " << age << endl;
+
+    //declaration
+    //int age;
+    //cout << age;
+
+    //defination
+    int age = 21;
+    cout << age <<endl;
+
+    //manipulation or updation
+    age = 101;
+    cout << age << endl;
+
+    return 0;
+}
