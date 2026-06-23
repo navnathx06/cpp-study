28dcb37917d3ebd50f1889246d2af4c0f4dbb880
Navnath <navnyajade@gmail.com>
2026-06-23 18:35:28 +0530

    Add main.cpp for user input example

---
+++ 3-user-input/theory/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    //variable declare
+    int marks;
+    cout << "Enter your marks : " ;
+
+    //input
+    cin >> marks;
+
+    cout << "Your Marks : " << marks << endl;
+
+    return 0;
+}
