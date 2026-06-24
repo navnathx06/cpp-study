d7273b6c824de42ca4d1f689dbad91a8170b9394
Navnath <navnyajade@gmail.com>
2026-06-24 10:31:42 +0530

    Add problem on variables and datatypes

--- 2-variables-datatypes/problems/problem1.cpp
+++ 2-variables-datatypes/problems/problem1.cpp
@@ -1,1 +1,21 @@
+//Write a program to declare and print variables of different data types.
 
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int age = 20;
+    float marks = 85.5;
+    char grade = 'A';
+    double price = 99.99;
+    bool pass = true;
+
+    cout << age << endl;
+    cout << marks << endl;
+    cout << grade << endl;
+    cout << price << endl;
+    cout << pass << endl;
+
+    return 0;
+}
