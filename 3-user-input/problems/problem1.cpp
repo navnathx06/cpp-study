8c4337d57dc13f08fef2626091a0e136a14067ed
Navnath <navnyajade@gmail.com>
2026-06-24 10:32:48 +0530

    Add problem

--- 3-user-input/problems/problem1.cpp
+++ 3-user-input/problems/problem1.cpp
@@ -1,1 +1,17 @@
 
+//Write a program to take integer input and display it.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num;
+
+    cout << "Enter number: ";
+    cin >> num;
+
+    cout << "Number is: " << num;
+
+    return 0;
+}
