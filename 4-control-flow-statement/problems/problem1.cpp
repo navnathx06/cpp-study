068423ddb2d0e825d87eb434e66c50931990436f
Navnath <navnyajade@gmail.com>
2026-06-24 10:38:20 +0530

    Add problem on if statement

--- 4-control-flow-statement/problems/problem1.cpp
+++ 4-control-flow-statement/problems/problem1.cpp
@@ -1,1 +1,17 @@
+//Q.Check whether number is positive
 
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num;
+    cin >> num;
+
+    if(num > 0)
+    {
+        cout << "Positive";
+    }
+
+    return 0;
+}
