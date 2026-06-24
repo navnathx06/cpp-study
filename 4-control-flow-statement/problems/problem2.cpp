fa776a8e31659b9a40dc2d8594b0138764496dd7
Navnath <navnyajade@gmail.com>
2026-06-24 10:43:01 +0530

    Add problem on if-else-if

---
+++ 4-control-flow-statement/problems/problem2.cpp
@@ -0,0 +1,54 @@
+//Q.Find grade according to marks
+
+int main()
+{
+    int marks;
+    cin >> marks;
+
+    if(marks >= 90)
+    {
+        cout << "Grade A";
+    }
+    else if(marks >= 75)
+    {
+        cout << "Grade B";
+    }
+    else if(marks >= 50)
+    {
+        cout << "Grade C";
+    }
+    else
+    {
+        cout << "Fail";
+    }
+
+    return 0;
+}
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int marks;
+    cin >> marks;
+
+    if(marks >= 90)
+    {
+        cout << "Grade A";
+    }
+    else if(marks >= 75)
+    {
+        cout << "Grade B";
+    }
+    else if(marks >= 50)
+    {
+        cout << "Grade C";
+    }
+    else
+    {
+        cout << "Fail";
+    }
+
+    return 0;
+}
